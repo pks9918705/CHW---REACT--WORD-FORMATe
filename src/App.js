015f9807af54a96d6da3a5d714da13395fa5baca
Priyanshu Kumar Singh <pks9918705@gmail.com>
2023-06-05 19:14:26 +0530

    Lec16 - Done - React Router is added

--- src/App.js
+++ src/App.js
@@ -3,49 +3,51 @@
 import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
+import TextForm from './components/TextForm';
 import React, { useState } from 'react';
+//react router
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+
 
 
 function App() {
 
   //state for holding dark
-  const [mode,setMode]=useState("light")
-   
-//function for handlng Toggle BUTTON
-const toggleButton =() => {
-  
-  if(mode === "light"){
-    setMode('dark')
+  const [mode, setMode] = useState("light")
+
+  //function for handlng Toggle BUTTON
+  const toggleButton = () => {
+
+    if (mode === "light") {
+      setMode('dark')
+    }
+    else setMode('light')
   }
-  else setMode('light')
-}
 
 
   return (
     <>
 
-
-      {/* Navbar */}
-      <Navbar
+     <BrowserRouter>
+     <Navbar
         title="Codiel"
         mode={mode}
         toggleButton={toggleButton}
-       
-        
-      />
-      {/* <Navbar /> */}
-      {/* form */}
-      {/* <div className="container">
-      <TextForm heading="Enter the text to analyse" />
-      </div> */}
-       
-      {/* <TextForm heading="Enter the text to analyse" /> */}
-
-      <About mode={mode}/>
-
-       
-      
+    />
+      <Routes>
+        {/* exact is used to match the exact route  */}
+        <Route exact path="/about" element={  <About mode={mode} />} />
+        <Route exact path="/" element={ <TextForm heading="Enter the text to analyse" />} />
+      </Routes>
+    </BrowserRouter>
+     
     </>
   );
 }
