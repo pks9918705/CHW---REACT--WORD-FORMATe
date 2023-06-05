5e343e1a441dfaa9a1d25f5f8d49807104810022
Priyanshu Kumar Singh <pks9918705@gmail.com>
2023-06-05 12:55:11 +0530

    Lec09 - Done - Adding more Feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar(props) {
     
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <div className="container-fluid">
       <a className="navbar-brand" href="/">{props.title}</a>
        
@@ -27,7 +27,7 @@ export default function Navbar(props) {
         </ul>
         <form className="d-flex">
           <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-outline-success" type="submit">Search</button>
+          <button className="btn btn-primary" type="submit">Search</button>
         </form>
       </div>
     </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,37 +1,63 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 
 
 export default function TextForm(props) {
 
-  const [text,setText]=useState("Enter text here")
+  const [text, setText] = useState("")
 
 
-  const handleUpClick=()=>{
+  const handleUpClick = () => {
     console.log('Button clicked');
     setText("You have clicked on Button")
-    let newText=text.toUpperCase()
+    let newText = text.toUpperCase()
     setText(newText)
-    
+
   }
-   
-  const handleOnChange=(event) => {
+
+  const handleOnChange = (event) => {
     console.log('On change');
-  
+
     setText(event.target.value)
 
-    
+  }
+  const handleLoClick = () => {
+    let newText = text.toLowerCase()
+    setText(newText)
 
   }
+  const handleToRev = () => {
+
+    var reversedString = '';
+    for (let i = text.length - 1; i >= 0; i--) {
+      reversedString += text[i];
+    }
+
+    let newText =  reversedString
+    setText(newText)
+     
+  }
+
+
+
   return (
     <div className="container my-5">
       <h1>{props.heading}</h1>
       <div className="mb-3">
-        
+
         <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
       </div>
-     
+
       <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+      <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
+      <button className="btn btn-primary mx-2" onClick={handleToRev}>Reverse Text</button>
+
+      {/* counter  */}
+      <div className="container my-5">
+        <h1>Your Text Summary</h1>
+        <p>{text.split(" ").length - 1} Words and {text.length} Characters</p>
+        <p>{0.16 * (text.split(" ").length - 1)} Min required to read</p>
+      </div>
 
     </div>
 
